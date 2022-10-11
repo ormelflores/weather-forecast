ca0cd6d257af41bbf90e6c1b1434063ef494e37b
ormelflores <floresormel@gmail.com>
2022-10-11 16:42:14 +0800

    #1 - changed api

--- resources/views/livewire/forecast-weather.blade.php
+++ resources/views/livewire/forecast-weather.blade.php
@@ -15,18 +15,20 @@
         </div>
         @if(filled($forecast))
             <div class="lg:text-center">
-                <p class="mt-2 text-3xl font-bold leading-8 tracking-tight sm:text-4xl">{{ $forecast['city'] }} - {{ $forecast['state'] }} - {{ $forecast['country'] }}</p>
+                <p class="mt-2 text-3xl font-bold leading-8 tracking-tight sm:text-4xl">{{ $forecast['city']->name }} - {{ $forecast['city']->country }}</p>
                 
                 <p class="mt-4 max-w-2xl text-xl text-white lg:mx-auto">Week's Forecast</p>
             </div>
 
             <div class="grid gap-4 mb-8 md:grid-cols-7 xl:grid-cols-4 mt-4">
-                @if(count($forecast['forecast']) > 0)
-                    @foreach ($forecast['forecast'] as $data)
+                @if(count($forecast['list']) > 0)
+                    @foreach(collect($forecast['list'])->groupBy(function ($date) {
+                        return date('l, F d', strtotime($date->dt_txt));
+                    }) as $key => $data)
                         <x-cards.weather
-                            title="{{$data->weekday}}, {{date('F d', strtotime($data->utcTime))}}"
-                            temperature="{{ $data->comfort }}"
-                            icon="{{$data->iconLink}}">
+                            title="{{$key}}"
+                            temperature="{{ $data[0]->main->temp }}"
+                            icon="https://openweathermap.org/img/wn/{{$data[0]->weather[0]->icon}}.png">
                         </x-cards.weather>
                     @endforeach
                 @endif
